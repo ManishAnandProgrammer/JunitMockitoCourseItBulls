ef5634761b0420aef56afdb736f3ca97cde48c2b
Manish Anand <manishanandprogrammer@gmail.com>
2023-03-12 13:27:36 +0530

    First Test Suite Added

--- src/test/java/com/manish/calculator/CalculatorTest.java
+++ src/test/java/com/manish/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
-package com.manish;
+package com.manish.calculator;
 
+import com.manish.Calculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
---
+++ src/test/java/com/manish/testsuite/MoneyTransferAndCalculatorTestSuite.java
@@ -0,0 +1,9 @@
+package com.manish.testsuite;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"com.manish.calculator", "com.manish.moneytransfer"})
+public class MoneyTransferAndCalculatorTestSuite {
+}
